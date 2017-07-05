dad25cefcfc614271ba7ab9ad401e5ada978ee19
Vinay Sridhar <vinay.sridhar@parallelmachines.com>
2017-07-04 23:35:36 -0700

    Print the actual results
    Takes in the input word lengths and does a combinatorial search of all lists of the required word lengths

--- src/WordBrain.java
+++ src/WordBrain.java
@@ -12,11 +12,8 @@ public class WordBrain {
 
 	private Map<Character, List<String>> dictMap;
 
-	private ArrayList<Thread> threadArr;
-
 	private WordBrain() throws Exception {
 		dictionary = new File("/usr/share/dict/words");
-		threadArr = new ArrayList<>();
 		setupDict();
 	}
 
@@ -35,12 +32,17 @@ public class WordBrain {
 		return bucket != null && bucket.contains(key);
 	}
 
-	private void printWords(String[][] inputArr, int maxLen) throws Exception {
+	private List<String> printWords(String[][] inputArr, int maxLen) throws Exception {
+		ArrayList<Thread> threadArr = new ArrayList<>();
+		ArrayList<FindWords> objList = new ArrayList<>();
+		ArrayList<String> results = new ArrayList<>();
+
 		for (int i = 0; i < inputArr.length; i++) {
 			for (int j = 0; j < inputArr[i].length; j++) {
 				FindWords findWords = new FindWords(inputArr, i, j, maxLen);
 				Thread t = new Thread(findWords);
 				threadArr.add(t);
+				objList.add(findWords);
 				t.start();
 			}
 		}
@@ -48,6 +50,59 @@ public class WordBrain {
 		for (Thread t : threadArr) {
 			t.join();
 		}
+
+		for (FindWords findWords : objList) {
+			results.addAll(findWords.getResults());
+		}
+
+		return results;
+	}
+
+	private void findUniqueN(Map<String, Integer> inputStringMap, Map<Integer, List<String>> allResults) {
+		int numLists = allResults.values().size();
+
+		int[] indexArr = new int[numLists];
+
+		for (int bitMap = 0; bitMap < Math.pow(10,  numLists); bitMap++) {
+			// Place each digit in the index array
+			int i = numLists - 1;
+			int tmp = bitMap;
+			while (tmp != 0) {
+				indexArr[i--] = tmp % 10;
+				tmp = tmp / 10;
+			}
+			while (i >= 0) {
+				indexArr[i--] = 0;
+			}
+
+			Map<String, Integer> inputMap = new HashMap<>(inputStringMap);
+			validateCombination(inputMap, allResults, indexArr);
+		}
+	}
+
+	private void validateCombination(Map<String, Integer> inputStringMap, Map<Integer, List<String>> allResults,
+									   int[] indexArr) {
+		List<List<String>> stringList = new ArrayList<>();
+		stringList.addAll(allResults.values());
+		for (int i = 0; i < indexArr.length; i++) {
+			String string = stringList.get(i).get(indexArr[i]);
+
+			for (Character ch : string.toCharArray()) {
+				String s = "" + ch;
+				int count = inputStringMap.get(s);
+				if (count == 0) {
+					return;
+				}
+				inputStringMap.put(s, count - 1);
+			}
+		}
+
+		System.out.println("Found a valid combination!");
+
+		for (int i = 0; i < indexArr.length; i++) {
+			System.out.print(stringList.get(i).get(indexArr[i]) + " ");
+		}
+		System.out.println();
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -55,18 +110,36 @@ public class WordBrain {
 		WordBrain wordBrain = new WordBrain();
 
 		String[][] inputArrA = new String[][] {
-				{ "w", "a" },
-				{ "y", "r" },
-				{ "t", "k" },
-				{ "e", "s" }
+				{ "y", "d", "o", "l" },
+				{ "a", "p", "i", "a" },
+				{ "r", "p", "r", "d" },
+				{ "s", "e", "e", "m" }
 		};
 
-		final long startTime = System.currentTimeMillis();
-		wordBrain.printWords(inputArrA, 5);
-		final long endTime = System.currentTimeMillis();
+		//int[] lengths = { 4, 5, 6 };
+		int[] lengths = { 5, 6 };
+		Map<String, Integer> inputStringMap = new HashMap<>();
+		for (String[] strings : inputArrA) {
+			for (String string : strings) {
+				Integer count = inputStringMap.get(string);
+				if (count == null) {
+					count = 0;
+				}
+				inputStringMap.put(string, count + 1);
+			}
+		}
 
-		System.out.println("Word search complete!");
-		System.out.println("Took " + (endTime - startTime) + "ms");
+		List<String> results = null;
+		Map<Integer, List<String>> allResults = new HashMap();
+		int prevLen = -1;
+		for (int length : lengths) {
+			if (length != prevLen) {
+				results = wordBrain.printWords(inputArrA, length);
+			}
+			prevLen = length;
+			allResults.put(length, results);
+		}
+		wordBrain.findUniqueN(inputStringMap, allResults);
 	}
 
 	private class FindWords implements Runnable {
@@ -77,6 +150,7 @@ public class WordBrain {
 		int maxLen;
 		ArrayList<String> buf;
 		boolean[][] visited;
+		ArrayList<String> results;
 
 		FindWords(String[][] inputArr, int i, int j, int maxLen) {
 			this.inputArr = inputArr;
@@ -84,16 +158,20 @@ public class WordBrain {
 			jIndex = j;
 			this.maxLen = maxLen;
 			buf = new ArrayList<>();
+			results = new ArrayList<>();
 			visited = new boolean[inputArr.length][inputArr[i].length];
 		}
 
 		public void run() {
 			try {
-				System.out.println("Running thread " + Thread.currentThread().getId());
 				printWords(inputArr, iIndex, jIndex, buf, maxLen, visited);
 			} catch (Exception e) {
 				System.out.println(e);
 			}
+		}
+
+		List<String> getResults() {
+			return results;
 		}
 
 		private void printWords(String[][] inputArr, int i, int j, ArrayList<String> buf,
@@ -113,7 +191,7 @@ public class WordBrain {
 				}
 
 				if (lookupDict(sb.toString())) {
-					System.out.println("FOUND! - " + sb);
+					results.add(sb.toString());
 				}
 
 				buf.remove(buf.size() - 1);
